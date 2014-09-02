809e5def048f26e271e3c02319a6f9e38fb05086
hovo1990 <hovakim_grabski@yahoo.com>
2014-09-02 19:31:40 +0400

    added GROM.py

---
+++ GROM.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of NINJA-IDE (http://ninja-ide.org).
+#
+# GROM is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# GROM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GROM; If not, see <http://www.gnu.org/licenses/>.
+
+
+
+###############################################################################
+# DOC
+###############################################################################
+
+"""GROM is Text and Table Editor for GROMACS(.itp,.top,.mdp) and
+"""
+
+
+"""
+GROM
+-----
+
+GROM is a GROMACS(mdp,itp,top) and PDB Editor in one package  based on Qt5,PyQt5 and Python3.
+It's GPL licensed!(See LICENSE)
+
+
+Before you run it:
+`````````````````
+
+    Please Install Qt5, Pyqt5 and Python3:
+        On Ubuntu:
+            sudo apt-get install build-essential
+            sudo apt-get install python3
+            sudo apt-get install qt5-default qttools5-dev-tools
+            sudo apt-get install python3-pyqt5
+
+And run it:
+
+.. code:: bash
+    $ python3 GROM.py
+
+"""
+###############################################################################
+# IMPORTS
+###############################################################################
+
+import grom
+
+###############################################################################
+# MAIN
+###############################################################################
+
+
+if __name__ == "__main__":
+    grom.setup_and_run()
